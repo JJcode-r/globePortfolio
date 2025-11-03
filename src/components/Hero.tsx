24e84e082d94e4aa5e74f6058b0f39486d21ff62
JJcode-r <joshuaigburu082@gmail.com>
2025-11-03 21:40:55 +0100

    Updated the X handle

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -245,7 +245,7 @@ const Hero: React.FC<HeroProps> = ({ titleRef, subtitleRef }) => {
   const socialLinks = [
     { Icon: Github, href: "https://github.com/JJcode-r/" },
     { Icon: Linkedin, href: "https://www.linkedin.com/in/joshua-igburu-7b178919a/" },
-    { Icon: Twitter, href: "https://x.com/IJ_the_Dev?t=rWYlDEtG1PnMrDCNpRoSNQ&s=09" },
+    { Icon: Twitter, href: "https://x.com/globe_the_dev?t=RO6MAOivsMGasX5H5XPZVA&s=09" },
   ];
 
   return (
